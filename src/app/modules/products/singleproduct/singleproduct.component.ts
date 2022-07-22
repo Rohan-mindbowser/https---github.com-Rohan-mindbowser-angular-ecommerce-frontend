bd547fba7493385560fc38f4cd8d108ea13e63b9
Rohan Kadam <rohan.kadam@mindbowser.com>
2022-07-22 16:25:41 +0530

    solved single-product undefined error

--- src/app/modules/products/singleproduct/singleproduct.component.ts
+++ src/app/modules/products/singleproduct/singleproduct.component.ts
@@ -16,21 +16,13 @@ export class SingleproductComponent implements OnInit {
   productCategory!: any;
   productId!: any;
   singleProduct!: any;
-  products!:any;
+  products!: any;
 
   ngOnInit(): void {
     this.productId = this.activeRouter.snapshot.params['id'];
 
     this._productApi.singleProduct(this.productId).subscribe((product) => {
       this.singleProduct = product[0];
-      console.log('Single Product:', this.singleProduct);
     });
-    this.assignProduct()
-  }
-
-  assignProduct(){
-    setTimeout(() => {
-      this.singleProduct = this.products
-    }, 1000);
   }
 }
